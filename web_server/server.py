0cea2ced5b45522c91a0d24eeb0594e4bb135c1e
Dmitriy Khodakov <dmitryhd@gmail.com>
2016-06-04 13:02:25 +0300

    static files serving

--- web_server/server.py
+++ web_server/server.py
@@ -2,23 +2,29 @@
 # -*- coding: utf-8 -*-
 
 """ """
+import os.path
 import tornado.ioloop
-import tornado.web
+import tornado.web as web
+
 
 PORT = 8888
 
+STATIC_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                           'static/')
 
-class IndexHandler(tornado.web.RequestHandler):
+
+class IndexHandler(web.RequestHandler):
     def get(self):
         self.write('a')
 
 
-def get_app() -> tornado.web.Application:
+def get_app() -> web.Application:
     """ Sets up routing"""
     routing = [
         (r"/", IndexHandler),
+        (r"/static/(.*)", web.StaticFileHandler, {"path": STATIC_PATH}),
     ]
-    app = tornado.web.Application(routing)
+    app = web.Application(routing)
     return app
 
 
@@ -29,4 +35,5 @@ def start_server():
 
 
 if __name__ == '__main__':
+    print(STATIC_PATH)
     start_server()
